83c379342f477c839050710f14ed9c841edd921e
张文川 <13522750636@163.com>
2022-01-17 16:47:38 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,26 @@
+const navbar = require('./navbar.js');
+module.exports = {
+  // 站点配置
+  lang: 'zh-CN',
+  title: '前端核心进阶',
+  description: '这是我的第一个 VuePress 站点',
+  head: [
+    ['link', { rel: 'icon', href: '/images/favicon.ico' }], //favicons，资源放在public文件夹
+    [
+      'meta',
+      {
+        name: 'keywords',
+        content: '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,Vue,React,python,css3,html5,Node,git,github,markdown',
+      },
+    ],
+  ],
+  editLink: false,
+  // 主题和它的配置
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+    navbar, // 顶部导航
+    // logo: '/images/logo.png', //左上侧 logo
+    repo: 'https://github.com/jslaowang',
+    editLink: false
+  },
+}
---
+++ docs/.vuepress/navbar.js
@@ -0,0 +1,27 @@
+// nav
+module.exports = [
+  {
+    text: 'JavaScript', link: '/pages/js/'  },
+  {
+    text: '框架',
+    children: [
+      { text: 'Vue', link: '/pages/vue/' },
+      { text: 'React', link: '/pages/react/' },
+    ],
+  },
+  {
+    text: 'Node',
+    children: [
+      { text: 'Node.js', link: '/pages/node/' }
+    ],
+  },
+  {
+    text: '工程化',
+    children: [
+      { text: 'webpack', link: '/pages/webpack/' },
+      { text: 'Vite', link: '/pages/vite/' },
+    ],
+  },
+  { text: '关于', link: '/pages/about/' },
+  { text: '收藏', link: '/pages/bookmark/'}
+]
